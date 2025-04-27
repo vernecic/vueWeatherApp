3e11471fb5d6f2c247f15bc627319e9bfa24f01b
vernecic <vidernecic1@gmail.com>
2025-04-27 23:17:44 +0200

    remove console.log

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import Display from "./components/Display.vue";
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const vrijemeData = ref(null);
-console.log(apiKey);
+
 const getWeather = async (city) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   const res = await fetch(url);
