fc48d6aa29d5134afd095594cc7e9a2ac4058808
vernecic <vidernecic1@gmail.com>
2025-04-27 23:12:36 +0200

    emoji change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,6 @@
     class="bg-blue-400 p-5 text-white text-3xl font-bold flex justify-between"
   >
     <p>Weather app</p>
-    <button class="mr-20 cursor-pointer">↻</button>
+    <button class="mr-20 cursor-pointer">☁️</button>
   </div>
 </template>
